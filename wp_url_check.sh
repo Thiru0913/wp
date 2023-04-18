50d0ac6ce9e107dddae8b5f834f99231df895658
Thiru0913 <34019615+Thiru0913@users.noreply.github.com>
2023-04-18 14:16:20 +0530

    wp_url_check script

---
+++ wp_url_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define the URL of your WordPress site
+site_url="https://your-site.com"
+
+# Send a cURL request to the site and store the HTTP response code in a variable
+response_code=$(curl --write-out %{http_code} --silent --output /dev/null $site_url)
+
+# Check if the response code indicates a successful request (200-299)
+if [ $response_code -ge 200 ] && [ $response_code -lt 300 ]; then
+  echo "WordPress site is loaded properly."
+else
+  echo "WordPress site is not loaded properly. Response code: $response_code"
+fi
